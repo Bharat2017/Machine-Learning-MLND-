8040d3974c43c39f6c68a56407e2c60a6489b7b2
Bharat Singh <bharatmca2010@gmail.com>
2017-08-26 00:24:37 +0400

    Add files via upload

---
+++ ND2-finding_donors/lambda.py
@@ -0,0 +1,10 @@
+# s1 = pd.get_dummies()
+# for x in range(10):
+#     s1.set_value(x, x ** 2)
+# print(s1)
+
+
+x = [2, 3, 4, 5, 6]
+y = [lambda v: v * 5 for v in x]
+for i, val in enumerate(a):
+    print(x)
